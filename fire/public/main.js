26e161b7cccac6f98e69083abf26b1777defdad4
ishino-kenta <berry.peach.97@gmail.com>
2018-10-16 11:38:10 +0900

    手札領域ができました

--- fire/public/main.js
+++ fire/public/main.js
@@ -12,6 +12,12 @@ phina.define('MainScene', {
         var label = phina.display.Label({text:"カードをドラッグで動かせます"});
         label.addChildTo(this);
         label.setPosition(320, 160);
+        // 手札領域
+        var hand_field = phina.display.RectangleShape();
+        hand_field.addChildTo(this);
+        hand_field.setPosition(320,860);
+        hand_field.setScale(10,3);
+        hand_field.fill = "pink";
         // データベースからカード生成
         var group = DisplayElement().addChildTo(this);
 //        firebase.database().ref("/pos/").on("child_added", function(snapshot) { 
@@ -19,14 +25,14 @@ phina.define('MainScene', {
 //            shape.addChildTo(group);
 //            card = Card(shape, snapshot);
 //        });
-
+        
         var pos1 = firebase.database().ref("/pos/c1/");
         var shape1 = phina.display.RectangleShape();
         shape1.addChildTo(group);
-        shape1.setScale(2,3);
+        //shape1.setScale(2,2);
         shape1.setInteractive(true);
         shape1.on('pointmove', function(e) {
-            shape1.x += e.pointer.dx;
+        shape1.x += e.pointer.dx;
             shape1.y += e.pointer.dy;
             pos1.set({x:this.x, y:this.y});
         });
@@ -38,7 +44,7 @@ phina.define('MainScene', {
         var pos2 = firebase.database().ref("/pos/c2/");
         var shape2 = phina.display.RectangleShape();
         shape2.addChildTo(group);
-        shape2.setScale(2,3);
+        //shape2.setScale(2,2);
         shape2.setInteractive(true);
         shape2.on('pointmove', function(e) {
             shape2.x += e.pointer.dx;
@@ -54,7 +60,6 @@ phina.define('MainScene', {
         this.update = function() {
            this.setRectInteraction();
         };
-        
         
         console.log(this.children);
         //ウィンドウ消した時
@@ -72,7 +77,9 @@ phina.define('MainScene', {
         this.group.children.each(function(rect, i) {
             self.group.children.each(function(target, j) {
                 // 重なっていて表示順が下のターゲットはタッチ不可にする
-                if (Collision.testRectRect(rect, target) && j < i) {
+                rec = phina.geom.Rect(rect.x, rect.y, rect.width, rect.height);
+                tar = phina.geom.Rect(target.x, target.y, target.width, target.height);
+                if (Collision.testRectRect(rec, tar) && j < i) {
                     target.setInteractive(false);
                 }
             });
