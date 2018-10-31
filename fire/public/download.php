1055e24eee0a241db6c23cb8b57dc28ee5871887
ishino-kenta <berry.peach.97@gmail.com>
2018-11-01 01:00:22 +0900

    downloadとimageを777にしました

--- fire/public/download.php
+++ fire/public/download.php
@@ -1,6 +1,7 @@
 <?php
     $url=$_POST["data"];
     $data = file_get_contents($url);
-    file_put_contents('./image/rock.jpg',$data);
-    print "endooooooooooooo";
+    $filename='./image/rock.jpg';
+    file_put_contents($filename,$data);
+    echo "good?";
 ?>
