2f7c5b38947aadbe08a2503603b431bc54496b88
ishino-kenta <berry.peach.97@gmail.com>
2018-11-20 10:56:31 +0900

    リファクタリング：コメントをつけました

--- fire/public/main.js
+++ fire/public/main.js
@@ -6,6 +6,7 @@ let ASSETS = {
     },
 };
 
+//ゲームシーン=================================================================
 phina.define('GameScene', {
     superClass: 'phina.display.DisplayScene',
     init: function (param) {
@@ -13,14 +14,14 @@ phina.define('GameScene', {
         this.backgroundColor = 'lightblue';
         const self = this;
         var Cards = [];
-        //IDの生成
+    //IDの生成
         var ID = Math.round(Math.random() * 1000000);
         var user = firebase.database().ref("/users").push({ id: ID });
-        //説明文の表示
-        var label = phina.display.Label({ text: "カードをドラッグで動かせます" });
+    //説明文の表示
+        var label = phina.display.Label({ text: "カードをドラッグで動かせます。¥nピンクのところは手札" });
         label.addChildTo(this);
         label.setPosition(320, 160);
-        // 手札領域の追加
+    // 手札領域の追加
         var hand_field = phina.display.RectangleShape();
         hand_field.addChildTo(this);
         hand_field.setPosition(320, 860);
@@ -29,15 +30,14 @@ phina.define('GameScene', {
         var hand = phina.geom.Rect(0, hand_field.y - hand_field.height / 2, hand_field.width * 10, hand_field.height * 10);
         var i = 1;
         var serect = null;
-        //部屋名の表示
+    //部屋名の表示
         param.room.ref.once('value').then(function (snapshot) {
             var label = phina.display.Label({ text:  snapshot.val().name});
             label.addChildTo(self);
             label.setPosition(320, 80);
         });
 
-        // データベースからカード生成
-        //親
+    // データベースからカード生成
         var group = DisplayElement().addChildTo(this);
         var poss = []; //場所の参照をまとめておく。
 
@@ -49,7 +49,7 @@ phina.define('GameScene', {
             var id1;
             shape.addChildTo(group);
             shape.setInteractive(true);
-            //ドラック時処理
+        //ドラック時処理
             shape.on('pointmove', function (e) {
                 if (id1 == 0 || id1 == ID) {
                     if(self.serect == this){                 
@@ -73,7 +73,7 @@ phina.define('GameScene', {
                 //self.setRectInteraction();
                 self.serect = null;
             });
-            //データベース書き換えた時の処理
+        //データベース書き換えた時の処理
             pos.on("value", function (snapshot) {
                 id1 = snapshot.val().belong;
                 shape.setPosition(snapshot.val().x, snapshot.val().y);
@@ -98,9 +98,9 @@ phina.define('GameScene', {
         
         this.group = group;
 
-        //ウィンドウ消した時の処理
+    //ウィンドウ消した時の処理
         window.onbeforeunload = function () {
-            //ユーザを消して手札を解放する
+        //ユーザを消して手札を解放する
             user.remove();
             for (let p of poss) {
                 p.once('value').then(function (snapshot) {
@@ -114,150 +114,22 @@ phina.define('GameScene', {
 
     update: function () {
     },
-    /*
-    //重なりの処理
-    setRectInteraction: function () {
-        // 全体を一旦タッチ可能にする
-        this.group.children.each(function (rect) {
-            rect.setInteractive(true);
-        });
-        var self = this;
-        // グループ総当たりで重なり具合に応じてタッチ可否を設定する
-        this.group.children.each(function (rect, i) {
-            self.group.children.each(function (target, j) {
-                // 重なっていて表示順が下のターゲットはタッチ不可にする
-                if (Collision.testRectRect(rect, target) && j < i) {
-                    target.setInteractive(false);
-                }
-            });
-        });
-    }*/
-});/*
-phina.define('Card', {
-    // 初期化
-    init: function(obj,b,c) {
-        // クラスメンバ 
-        let belong = 0;
-        let id = b;
-        let card = obj;
-        let start_X;
-        let start_Y;
-        let dir_X;
-        let dir_Y;
-        card.scaleX = 0.6;
-        card.scaleY = 0.6;
-        card.x = 250;
-        card.y = 300;      
-        card.setInteractive(true);
-        card.onpointstart = function(e) {
-          self.serect = this;
-          self.tauch = true;
-          this.remove();
-          card.addChildTo(c);        
-          start_X = e.pointer.x;
-          start_Y = e.pointer.y;                          
-        };
-        card.onpointend = function(e) {
-          dir_X = e.pointer.x - start_X;
-          dir_Y = e.pointer.y - start_Y;
-          if((dir_X > -0.01) && (dir_X < 0.01))
-          {
-            if((dir_X > -0.01) && (dir_X < 0.01))
-            {            
-              back.addChildTo(c);
-              this.remove();
-            }
-          }
-          self.serect = null;
-        };
-        card.on('pointmove', function(e) {
-            if (belong == 0 || belong == ID) {
-                if(self.serect == this) 
-                {
-                    card.x += e.pointer.dx;
-                    card.y += e.pointer.dy;
-                    back.x += e.pointer.dx;
-                    back.y += e.pointer.dy;
-                    if (Collision.testRectRect(obj, hand)) {
-                        pos1.update({ belong: id, x: card.x, y: card.y });
-                    } else {
-                        pos1.update({ belong: 0, x: card.x, y: card.y });
-                    } 
-                }        
-            }       
-          
-        });
-        card.update = function() 
-        {
-          if(self.serect == null || self.serect == this)
-          {
-            card.setInteractive(true);
-          } else {
-            card.setInteractive(false);
-          }
-        }
-        let back = obj;
-        back.scaleX = 0.6;
-        back.scaleY = 0.6;
-        back.x = 250;
-        back.y = 300;
-        back.addChildTo(c);
-        back.setInteractive(true); 
-        back.onpointstart = function(e) {
-          self.serect = this;
-          this.remove();
-          back.addChildTo(c);
-          start_X = e.pointer.x;
-          start_Y = e.pointer.y; 
-        };
-        back.onpointend = function(e) {
-          dir_X = e.pointer.x - start_X;
-          dir_Y = e.pointer.y - start_Y;
-          if(dir_X == 0)
-          {
-            if(dir_Y == 0)
-            {
-              card.addChildTo(c);
-              this.remove();
-            }
-          }
-          self.serect = null;
-        };
-        back.on('pointmove', function(e) {
-          if(self.serect == this) 
-          {
-            card.x += e.pointer.dx;
-            card.y += e.pointer.dy;
-            back.x += e.pointer.dx;
-            back.y += e.pointer.dy; 
-          }               
-        });
-        back.update = function()
-        {
-          if(self.serect == null || self.serect == this)
-          {          
-            back.setInteractive(true);
-          } else {
-            back.setInteractive(false);
-          }
-        }
-    },
-});*/
+});
 
-//タイトルメニュー
+//タイトルシーン=================================================================
 phina.define('TitleScene', {
     superClass: 'phina.display.DisplayScene',
     init: function () {
         this.superInit();
-        this.backgroundColor = "rgb(39,86,3)";
         const self = this;
-
+    //タイトルをデコる
+        this.backgroundColor = "rgb(39,86,3)";
         var shape = phina.display.Sprite("title");
         shape.setPosition(this.gridX.center(), this.gridY.span(10));
         shape.addChildTo(this);
     
-        var bool = false;
-
+        var bool = false;   //画像のロードが終わったか確認用。つまりゴミ
+    //ボタン
         Button({
             text: "enter",
             fontSize: 60,
@@ -265,7 +137,7 @@ phina.define('TitleScene', {
         ).addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(10)).onpush = function () {
             self.exit('Room');
         };
-        console.log("make");
+
         var make = Button({
             text: "make",
             fontSize: 60,
@@ -277,6 +149,7 @@ phina.define('TitleScene', {
                 self.exit('Make');
             }
         };
+    //タイトル画面で全ての画像をダウンロードします。無駄です
         firebase.database().ref("/image").once('value').then(async function (snapshot) {
             const snapval = snapshot.val();
             const ret =  Promise.all(                      
@@ -306,7 +179,7 @@ phina.define('TitleScene', {
     }
 });
 
-//ルーム作成
+//ルーム作成シーン=================================================================
 phina.define('MakeScene', {
     superClass: 'phina.display.DisplayScene',
     init: function () {
@@ -315,6 +188,7 @@ phina.define('MakeScene', {
         self = this;
         var rnd = Math.round(Math.random() * 100000);
 
+    //選ぶためにカードを全部表示していく。
         var cards = [];
         var x = 1.5;
         var y = 1.5;
@@ -339,7 +213,7 @@ phina.define('MakeScene', {
                 cards.push(result);
             });
         }
-
+    //ボタン
         Button({
             text: "make",
             fontSize: 30,
@@ -358,13 +232,12 @@ phina.define('MakeScene', {
                     y: 100
                 });
             }
-            console.log("go");
             self.exit('Game', param);
         };
     }
 });
 
-//ルーム選択
+//ルーム選択シーン=================================================================
 phina.define('RoomScene', {
     superClass: 'phina.display.DisplayScene',
     init: function () {
@@ -372,8 +245,8 @@ phina.define('RoomScene', {
         this.backgroundColor = 'lightblue';
         const self = this;
         var i = 2;
-        var sel = false;
-        var param;
+        var sel = false;    //１度しかボタンを押させないため。
+    //部屋ごとにボタンを作る。
         firebase.database().ref("/room").on("child_added", function (snapshot) {
             var name = snapshot.val().name;
             Button({
@@ -384,7 +257,8 @@ phina.define('RoomScene', {
                 if (!sel) {
                     this.fill = "pink";
                     sel = true;
-                    param = { room: snapshot };
+                    var param = { room: snapshot };
+                //ルームのカードをダウンロードして終わったらスタート。
                     snapshot.child("/cards/").ref.once('value').then(async function (snapshot2) {
                         const snapval = snapshot2.val();                                       
                         const ret =  Promise.all(                      
@@ -418,7 +292,7 @@ phina.define('RoomScene', {
     }
 });
 
-//メイン処理
+//メイン処理=================================================================
 phina.main(function () {
     var app = GameApp({
         startLabel: 'Title',
